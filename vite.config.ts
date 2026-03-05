0d5764063bd71425de2f38d74cd5fd9e9d8ce513
Your Name <you@example.com>
2026-03-05 10:35:15 +0000

    Setup visualizer

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
+import { visualizer } from 'rollup-plugin-visualizer'
 
 export default defineConfig({
 plugins: [
 react(),
-tailwindcss()
+tailwindcss(),
+visualizer({ filename: 'stats.html' })
 ],
 base: '/react-starter-template/',
 })
